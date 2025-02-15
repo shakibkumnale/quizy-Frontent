9b4a4a59460e69f63e0bd2c757feac3dd691f10b
shakibkumnale <shakibkumnali@gmail.com>
2025-02-16 03:15:15 +0530

    token debugging 2

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(
     origin: process.env.CORS || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 

dd8e7b920bb158a69d5a2d725174612297aab81e
shakibkumnale <shakibkumnali@gmail.com>
2025-02-16 03:01:43 +0530

    cors permisssion edit

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 import express from 'express';
-import './utils/env.js';
+import dotenv from 'dotenv';
 import connectDB from './connection/connect.js';
 import { router } from './router/auth-router.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+// Load environment variables
+dotenv.config();
 
 const app = express();
 connectDB();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Update CORS configuration to allow requests from the frontend's origin
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
+// Configure CORS to allow all methods and dynamic origin
+app.use(
+  cors({
+    origin: process.env.CORS || 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 const port = process.env.PORT || 10211;
 
 app.use('/', router);
 
-app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}!`));
+app.listen(port, () =>
+  console.log(`Example app listening on port http://localhost:${port}!`)
+);
